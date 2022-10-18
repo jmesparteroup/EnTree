dc6a7f70006f4457fded5f9e3ac27b6df4591006
Joshua Espartero <jmespartero@up.edu.ph>
2022-10-19 06:51:21 +0800

    Changed DB Name to entree

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose
-  .connect("mongodb://mongo:27017/trees", {
+  .connect("mongodb://mongo:27017/entree", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
