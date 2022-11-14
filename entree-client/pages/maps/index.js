20d22b8ac332bf858be31a156c402083c33d2b2c
Joshua Espartero <jmespartero@up.edu.ph>
2022-11-14 18:17:50 +0800

    Next Client moved to arcgis

--- entree-client/pages/maps/index.js
+++ entree-client/pages/maps/index.js
@@ -1,12 +1,21 @@
 import dynamic from "next/dynamic";
 
 const EntreeMapWithNoSSR = dynamic(
-  () => import("../../components/display/Map"),
+  () => import("../../components/display/ArcGISMap"),
   {
     ssr: false,
   }
 );
 
 export default function Maps() {
-  return <EntreeMapWithNoSSR />;
+  return (
+    <div className="h-screen">
+      <div className="h-1/11 w-full">
+        <h1 className="text-center text-3xl my-2 font-bold">Map</h1>
+      </div>
+      <div className="h-5/6 w-full">
+        <EntreeMapWithNoSSR />
+      </div>
+    </div>
+  );
 }
