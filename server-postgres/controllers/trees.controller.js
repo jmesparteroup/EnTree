d61b8dbb2725a3f0550a61c4661c1260b8fe16b4
Tayan Gelera <tayangelera@gmail.com>
2022-12-03 18:05:31 +0800

    Improve query result format

--- server-postgres/controllers/trees.controller.js
+++ server-postgres/controllers/trees.controller.js
@@ -7,30 +7,26 @@ class TreesController {
 
     async createTree(req, res) {
         try {
-            console.log(req.body);
             const treeData = new this.TreeModel(req.body);
             const tree = await this.TreesRepository.createTree(treeData);
             res.status(200).json(tree);
         } catch (error) {
-            console.log(error);
             res.status(500).json(error);
         }
     }
 
     async getAllTrees(req, res) {
         try {
-            console.log("Getting all trees.");
             const trees = await this.TreesRepository.getTrees();
-            res.status(200).json(trees);
+            const result = trees.get_all_trees;
+            res.status(200).json(result);
         } catch (error) {
-            console.log(error);
             res.status(500).json(error);
         }
     }
 
     async getTreeById(req, res) {
         try {
-            console.log("Getting tree by id");
             const tree = await this.TreesRepository.getTree(req.params.id);
             res.status(200).json(tree);
         } catch (error) {
@@ -60,9 +56,9 @@ class TreesController {
 
     async getTreeByProximity(req, res) {
         try {
-            console.log("Scanning nearby trees")
             const trees = await this.TreesRepository.getTreeByProximity(req.body.longitude, req.body.latitude, req.body.radius);
-            res.status(200).json(trees);
+            const result = trees.j;
+            res.status(200).json(result);
         } catch (error) {
             res.status(500).json(error);
         }
@@ -70,13 +66,13 @@ class TreesController {
 
     async getTreeByCity(req, res) {
         try {
-            console.log("Viewing Trees by City");
             const city = req.body.city;
             if (!city) {
                 res.status(400).json({error:"City not found"}); 
             }
             const trees = await this.TreesRepository.getTreeByCity(city)
-            res.status(200).json(trees);
+            const result = trees.j;
+            res.status(200).json(result);
         } catch (error) {
             res.status(500).json(error);
         }
