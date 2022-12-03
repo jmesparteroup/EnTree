16d34f2fe394185cc5280b8dcf356f4563e286f1
Joshua Espartero <jmespartero@up.edu.ph>
2022-12-03 19:21:04 +0800

    Fixed zoom viewing

--- entree-client/stores/treesStore.js
+++ entree-client/stores/treesStore.js
@@ -1,10 +1,8 @@
 import create from "zustand";
 
 const useTreesStore = create((set) => ({
-  treesState: {
-    trees: [],
-  },
-  addTrees: (trees) => set((state) => ({ trees: [...state.trees, trees] })),
+  trees: [],
+  addTrees: (trees) => set((state) => ({ trees: [...state.trees, ...trees] })),
   clearTrees: () => set({ trees: [] }),
 }));
 
