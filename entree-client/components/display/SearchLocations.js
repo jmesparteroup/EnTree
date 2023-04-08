5b323de3d68a9bef5e99323f542be21de945d47b
Joshua Espartero <jmespartero@up.edu.ph>
2023-04-08 20:10:04 +0800

    Added on Enter on search

--- entree-client/components/display/SearchLocations.js
+++ entree-client/components/display/SearchLocations.js
@@ -4,24 +4,35 @@ import {
   MinusCircleIcon,
 } from "@heroicons/react/24/outline";
 
-const levenshteinDistance = (str1 = '', str2 = '') => {
-  const track = Array(str2.length + 1).fill(null).map(() =>
-  Array(str1.length + 1).fill(null));
+function debounce(callback, delay) {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
+}
+
+const levenshteinDistance = (str1 = "", str2 = "") => {
+  const track = Array(str2.length + 1)
+    .fill(null)
+    .map(() => Array(str1.length + 1).fill(null));
   for (let i = 0; i <= str1.length; i += 1) {
-     track[0][i] = i;
+    track[0][i] = i;
   }
   for (let j = 0; j <= str2.length; j += 1) {
-     track[j][0] = j;
+    track[j][0] = j;
   }
   for (let j = 1; j <= str2.length; j += 1) {
-     for (let i = 1; i <= str1.length; i += 1) {
-        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
-        track[j][i] = Math.min(
-           track[j][i - 1] + 1, // deletion
-           track[j - 1][i] + 1, // insertion
-           track[j - 1][i - 1] + indicator, // substitution
-        );
-     }
+    for (let i = 1; i <= str1.length; i += 1) {
+      const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+      track[j][i] = Math.min(
+        track[j][i - 1] + 1, // deletion
+        track[j - 1][i] + 1, // insertion
+        track[j - 1][i - 1] + indicator // substitution
+      );
+    }
   }
   return track[str2.length][str1.length];
 };
@@ -35,7 +46,9 @@ export default function SelectLocations({
 }) {
   const [openSearchBar, setOpenSearchBar] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
-  const setSelectedLocation = useSearchStore((state) => state.setSelectedLocation);
+  const setSelectedLocation = useSearchStore(
+    (state) => state.setSelectedLocation
+  );
   const setData = useSearchStore((state) => state.setData);
 
   const dummy_search = async (query) => {
@@ -43,35 +56,44 @@ export default function SelectLocations({
     if (!query) return;
 
     const sortedBasedOnLevenshteinDistance = locationList.sort((a, b) => {
-      
       if (a === b) return 0;
 
       const levenshteinWeight = 0.3;
       const isInWordWeight = 0.7;
 
-      const aIsInWordScore = a.toLowerCase().includes(query.toLowerCase()) ? -3 : 3;
-      const bIsInWordScore = b.toLowerCase().includes(query.toLowerCase()) ? -3 : 3;
+      const aIsInWordScore = a.toLowerCase().includes(query.toLowerCase())
+        ? -3
+        : 3;
+      const bIsInWordScore = b.toLowerCase().includes(query.toLowerCase())
+        ? -3
+        : 3;
 
       const aDistance = levenshteinDistance(a, query);
       const bDistance = levenshteinDistance(b, query);
 
-      const aScore = aDistance * levenshteinWeight + aIsInWordScore * isInWordWeight;
-      const bScore = bDistance * levenshteinWeight + bIsInWordScore * isInWordWeight;
+      const aScore =
+        aDistance * levenshteinWeight + aIsInWordScore * isInWordWeight;
+      const bScore =
+        bDistance * levenshteinWeight + bIsInWordScore * isInWordWeight;
 
       return aScore - bScore;
-    }
-    );
+    });
 
     // return top 5 results
     return sortedBasedOnLevenshteinDistance.slice(0, 5);
-
   };
+
+
 
   const searchLocations = async (e) => {
     // implement debounce time of 500ms
     const query = e.target.value;
+    console.log(query)
     // if query is empty return
-    if (!query) return;
+    if (!query) {
+      setSearchResults([]);
+      return;
+    }
 
     try {
       // search locations
@@ -79,6 +101,14 @@ export default function SelectLocations({
       setSearchResults(locations);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const debouncedSearch = debounce(searchLocations, 200);
+
+  const handleOnEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSelectLocation(searchResults[0]);
     }
   };
 
@@ -104,7 +134,8 @@ export default function SelectLocations({
               placeholder="Search Barangays"
               className="w-full h-10 px-4 focus:outline-none"
               autoComplete="off"
-              onChange={searchLocations}
+              onKeyDown={handleOnEnter}
+              onChange={debouncedSearch}
             />
             <div className="w-10 h-10 bg-[var(--primary-bg-color)] py-2 flex justify-center align-center">
               <MagnifyingGlassIcon className="h-6 w-6 flex justify-center align-center text-gray-500" />
